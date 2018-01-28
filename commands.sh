ba86f65b7714e833794553d9215017110259f1fe
turtlegood <turtlegood@163.com>
2018-01-28 11:09:07 +0800

    add: command of exporting all checkpoints to models

--- commands.sh
+++ commands.sh
@@ -4,7 +4,22 @@ data | build_data )
     python3 build_data.py --X_input_dir ./data/crop_has_aug --Y_input_dir ./data/crop_not_aug --X_output_file ./data/has.tfrecords --Y_output_file ./data/not.tfrecords
     ;;
 train )
-    python3 train.py --X ./data/has.tfrecords --Y ./data/not.tfrecords --image_size 48
+    python3 train.py \
+        --X ./data/has.tfrecords \
+        --Y ./data/not.tfrecords \
+        --image_size 48
+    ;;
+export )
+    for D in `find ./checkpoints/ -mindepth 1 -maxdepth 1 -type d`
+    do
+        echo exporting: $D
+        short="${D#./checkpoints/}"
+        python3 export_graph.py \
+            --checkpoint_dir checkpoints/$short \
+            --XtoY_model add-$short.pb \
+            --YtoX_model rem-$short.pb \
+            --image_size 48
+    done
     ;;
 * )
     echo "Wrong input"
