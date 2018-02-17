feat: new tanh; fix float->int overflow

generator.py
@@ -57,8 +57,12 @@ class Generator:
       # but actually tanh was used and no _norm here
       # XXX: Try to do a residual thing :/
       if self.FLAGS.use_G_resi:
-        output = tf.nn.tanh(input + ops.c7s1_k(u32, 3, norm=None,
-            activation=None, reuse=self.reuse, name='output'))           # (?, w, h, 3)
+        if self.FLAGS.use_G_new_tanh:
+          output = input + tf.nn.tanh(ops.c7s1_k(u32, 3, norm=None,
+              activation=None, reuse=self.reuse, name='output'))           # (?, w, h, 3)
+        else:
+          output = tf.nn.tanh(input + ops.c7s1_k(u32, 3, norm=None,
+              activation=None, reuse=self.reuse, name='output'))           # (?, w, h, 3)
       else:
         output = ops.c7s1_k(u32, 3, norm=None,
             activation='tanh', reuse=self.reuse, name='output')           # (?, w, h, 3)

utils.py
@@ -102,7 +102,8 @@ def summary_float_image(name, image, summary_histogram=True, \
 def convert2int(image):
   """ Transfrom from float tensor ([-1.,1.]) to int image ([0,255])
   """
-  return tf.image.convert_image_dtype((image+1.0)/2.0, tf.uint8)
+  # https://www.tensorflow.org/api_docs/python/tf/image/convert_image_dtype
+  return tf.image.convert_image_dtype((image+1.0)/2.0, tf.uint8, saturate=True)
 
 def convert2float(image):
   """ Transfrom from int image ([0,255]) to float tensor ([-1.,1.])